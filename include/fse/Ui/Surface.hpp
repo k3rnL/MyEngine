b7189e6de39bab9319eba962b964aa1811823edd
Erwan Daniel <erwan.daniel@epitech.eu>
2018-03-12 19:56:11 +0100

    typeid fix

--- include/fse/Ui/Surface.hpp
+++ include/fse/Ui/Surface.hpp
@@ -1,6 +1,6 @@
 /**
  * @Date:   2018-02-23T00:46:33+01:00
- * @Last modified time: 2018-02-23T01:04:03+01:00
+ * @Last modified time: 2018-03-12T19:55:55+01:00
  */
 #ifndef FSE_UI_SURFACE_HPP
 #define FSE_UI_SURFACE_HPP
@@ -100,7 +100,7 @@ namespace fse {
 
 			template <class T>
 			FSE_API_EXPORT T	*getBehavior() {
-				return (T *)behaviors[typeid T];
+				return (T *)behaviors[typeid (T)];
 			}
 
 			template <class T>
